9f72edb7f1eccca223bd62344a5eb0b06326da8f
Rijvan Mohammad <rijvan.mohammad@djtcorp.in>
2021-07-28 13:13:17 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import {useRef,useEffect} from "react";
 import './App.css';
 
 function App() {
+  let videoRef = useRef(null)
+
+  useEffect(()=>{
+    console.log(videoRef.current)
+  },[])
+  const onDragStart = (ev,id) => {
+      console.log('dragstart:',id);
+      videoRef.current.pause()
+      ev.dataTransfer.effectAllowed = "move";
+  }
+
+  const onDrop = (ev) => {
+    console.log('ondrop')
+    ev.stopPropagation();
+    ev.preventDefault();
+    ev.target.appendChild(videoRef.current);
+    ev.dataTransfer.dropEffect = "move"
+    videoRef.current.play()
+  }
+
+  const onDragOver = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="top-left" onDrop={onDrop} onDragOver = {onDragOver}>
+      
+      </div>
+      <div className="top-right" onDrop={onDrop} onDragOver = {onDragOver}></div>
+      <div className="bottom-left" onDrop={onDrop} onDragOver = {onDragOver}>
+        <video 
+          id="video"
+          className="video"
+          width="200" 
+          height="300" 
+          key="dragable-video"
+          ref={videoRef}
+          onDragStart = {(e) => onDragStart(e,"video")}
+          controls  
+          draggable>
+          <source src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4" type="video/mp4"/>
+        </video>
+      </div>
+      <div className="bottom-right" onDrop={onDrop} onDragOver = {onDragOver}></div>
     </div>
   );
 }
